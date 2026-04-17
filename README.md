3432060f34f6e4f208cab4e879b9c8612905d7ec
PEN <uemasaki20011222@gmail.com>
2026-04-17 23:07:10 +0900

    Holiday API

---
+++ README.md
@@ -0,0 +1,4 @@
+# GitHub Actionsを利用したAPIの生成
+
+## References
+> [holidays-jp](https://github.com/holidays-jp/holidays-jp.github.io)
---
+++ script/holiday.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+import requests
+from icalendar import Calendar
+import json
+
+class GenerateHolidayAPI:
+    def __init__(
+        self, 
+        url             = "https://calendar.google.com/calendar/ical/ja.japanese%23holiday%40group.v.calendar.google.com/public/basic.ics"
+    ):
+        r               = requests.get(url).text
+        self.cal        = Calendar.from_ical(r)
+        self.holidays   = {}
+        
+    def generate(self):
+        for component in self.cal.walk():
+          if component.name == "VEVENT":
+            date        = component.get('dtstart').dt.isoformat()
+            summary     = str(component.get('summary'))
+            self.holidays[date] = summary
+        
+        return self.holidays
+    
+    def dump(
+        self,
+        output          = "docs/holiday.json"
+    ):
+        with open(output, "w", encoding="UTF-8") as f:
+            json.dump(self.holidays, f, ensure_ascii=False, indent=4)
+
+if __name__=="__main__":
+    api = GenerateHolidayAPI()
+    api.generate()
+    api.dump()
